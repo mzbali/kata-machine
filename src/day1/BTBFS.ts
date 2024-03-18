351cc051f03d47ad8e0d0ebc98c91de8bcbf6899
Mahmud Zaman Bali <mahmudzbali@gmail.com>
2024-03-18 04:42:18 +0000

    feat: add dfs on tree three types

--- src/day1/BTBFS.ts
+++ src/day1/BTBFS.ts
@@ -1,3 +1,14 @@
 export default function bfs(head: BinaryNode<number>, needle: number): boolean {
+    const q = [head];
 
+    while (q.length != 0) {
+        const node = q.shift();
+        if (node && node.value === needle)
+            return true;
+        if (node && node.left)
+                q.push(node.left)
+        if (node && node.right)
+            q.push(node.right)
+    }
+    return false;
 }
--- src/day1/BTPreOrder.ts
+++ src/day1/BTPreOrder.ts
@@ -1,3 +1,18 @@
-export default function pre_order_search(head: BinaryNode<number>): number[] {
+function walk(curr: BinaryNode<number> | null, path: number[]) {
+    // Base Case
+    if (!curr) {
+        return path;
+    }
+    
+    // Pre
+    path.push(curr.value);
+    // Recursion
+    walk(curr.left, path);
+    walk(curr.right, path);
 
+    return path;
+}
+
+export default function pre_order_search(head: BinaryNode<number>): number[] {
+    return walk(head, []);
 }
